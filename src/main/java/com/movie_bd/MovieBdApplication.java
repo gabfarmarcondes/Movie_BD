cfe5ad25c46a938fabd7b729d8157792b9c10697
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2025-07-01 18:00:38 -0300

    iniciando a fase de desenvolvimento

---
+++ src/main/java/com/movie_bd/MovieBdApplication.java
@@ -0,0 +1,13 @@
+package com.movie_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie_bd/MovieBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
